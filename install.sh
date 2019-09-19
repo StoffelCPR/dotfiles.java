2864b705d7f4703a3b8a7246950dc38c729d5db3
Stoffel <stoffel2107@gmail.com>
2019-09-19 12:56:15 +0200

    Fix: fixing java binary installation
    Issue #1 related
    
    Installs verison 7,8,10,11,12,13
    9 is the only zip and throws an error.
    cjava cmd is not installed

--- install.sh
+++ install.sh
@@ -18,10 +18,14 @@ filepath=$( cd "$(dirname "$0")" ; pwd )
 
 declare -a jdk_urls=(https://download.java.net/openjdk/jdk7u75/ri/openjdk-7u75-b13-linux-x64-18_dec_2014.tar.gz https://download.java.net/openjdk/jdk8u40/ri/openjdk-8u40-b25-linux-x64-10_feb_2015.tar.gz https://download.java.net/openjdk/jdk9/ri/openjdk-9+181_linux-x64_ri.zip https://download.java.net/openjdk/jdk10/ri/openjdk-10+44_linux-x64_bin_ri.tar.gz https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz https://download.java.net/openjdk/jdk12/ri/openjdk-12+32_linux-x64_bin.tar.gz https://download.java.net/openjdk/jdk13/ri/openjdk-13+33_linux-x64_bin.tar.gz)
 
+echo -e "${YELLOW} Trying to create temporary download directory${NOCOLOR}"
+mkdir ./java_binaries && echo -e "${GREEN}...done${NOCOLOR}"
+
 I=7
 for url in "${jdk_urls[@]}" ; do
+  break
     echo -e "${YELLOW}Processing JDK ${I}${NOCOLOR}"
-    curl -o java${I} ${url}
+    curl -o ./java_binaries/java${I} ${url}
     I=$((${I} + 1))
 done
 
@@ -31,36 +35,49 @@ if [[ ${OS} == Debian ]]; then
         echo -e "${YELLOW}Creating java directory${NOCOLOR}"
         [[ $(sudo mkdir ./jtest) ]] && echo -e "${GREEN}...done${NOCOLOR}"
     } || {
-        echo -e "${YELLOW}Extracting & Moving JDK 7 to /var/lib/java/java7...${NOCOLOR}"
-        sudo mkdir java7-1
-        sudo tar xvzf openjdk-7u75-b13-linux-x64-18_dec_2014.tar.gz
-        #sudo mv jdk-7u80 /var/lib/java/java7
-        echo -e "${GREEN}...done${NOCOLOR}"
-        echo "Extracting & Moving JDK 8 to /var/lib/java/java8..."
-        sudo tar xvzf openjdk-8u40-b25-linux-x64-10_feb_2015.tar.gz
-        #sudo mv jdk1.8.0_201 /var/lib/java/java8
-        echo "...done"
-        echo "Extracting & Moving JDK 9 to /var/lib/java/java9..."
-        sudo unzip openjdk-9+181_linux-x64_ri.zip
-        #sudo mv jdk-9.0.4 /var/lib/java/java9
-        echo "...done"
-        echo "Extracting & Moving JDK 10 to /var/lib/java/java10..."
-        sudo tar xvzf openjdk-10+44_linux-x64_bin_ri.tar.gz
-        #sudo mv jdk-10.0.2_linux-x64_bin.tar.gz /var/lib/java/java10
-        echo "...done"
+        JAVA_VERSION=7
+        DIRECTORY="/var/lib/java/"
+        echo -e "${YELLOW}Checking for existing java installations${NOCOLOR}"
+        while [[ ${JAVA_VERSION} -le 13 ]]; do
+          fullpath="${DIRECTORY}java${JAVA_VERSION}"
+
+          if [[ -d "/var/lib/java/java${JAVA_VERSION}" ]]; then
+            read -r -p "Java${JAVA_VERSION} already exists do you want to override it? [Y/n]" input
+            case ${input} in
+                [yY][eE][sS]|[yY])
+                    echo -e "${YELLOW}Removing existing jdk in ${DIRECTORY}java${JAVA_VERSION}${NOCOLOR}"
+                    sudo rm -rf ${DIRECTORY}java${JAVA_VERSION} && echo -e "${GREEN}...done${NOCOLOR}"
+                    echo -e "${YELLOW}Creating final JDK directory in /var/lib/java for Java ${JAVA_VERSION}${NOCOLOR}"
+                    sudo mkdir ${fullpath} && echo -e "${GREEN}...done${NOCOLOR}"
+                    echo -e "${YELLOW}Extracting & moving downloaded java${JAVA_VERSION} binaries to ${DIRECTORY}java${JAVA_VERSION}${NOCOLOR}"
+                    sudo tar xzf ./java_binaries/java${JAVA_VERSION} -C ${DIRECTORY}java${JAVA_VERSION} --strip-components 1 && echo -e "${GREEN}...done${NOCOLOR}"
+                    JAVA_VERSION=$(($JAVA_VERSION+1))
+                    continue
+                ;;
+                [nN][oO]|[nN])
+                    echo -e "${RED}Not installing ${UNDERLINED}java${JAVA_VERSION}${NORMAL}${NOCOLOR}"
+                    JAVA_VERSION=$(($JAVA_VERSION+1))
+                    continue
+                ;;
+                *)
+                echo -e "${YELLOW}Removing existing jdk in ${DIRECTORY}java${JAVA_VERSION}${NOCOLOR}"
+                sudo rm -rf ${DIRECTORY}java${JAVA_VERSION} && echo -e "${GREEN}...done${NOCOLOR}"
+                echo -e "${YELLOW}Creating final JDK directory in /var/lib/java for Java ${JAVA_VERSION}${NOCOLOR}"
+                sudo mkdir ${fullpath} && echo -e "${GREEN}...done${NOCOLOR}"
+                echo -e "${YELLOW}Extracting & moving downloaded java${JAVA_VERSION} binaries to ${DIRECTORY}java${JAVA_VERSION}${NOCOLOR}"
+                sudo tar xzf ./java_binaries/java${JAVA_VERSION} -C ${DIRECTORY}java${JAVA_VERSION} --strip-components 1 && echo -e "${GREEN}...done${NOCOLOR}"
+                JAVA_VERSION=$(($JAVA_VERSION+1))
+                continue
+                ;;
+                esac
+          fi
+          echo -e "${YELLOW}Creating final JDK directory in /var/lib/java for Java ${JAVA_VERSION}${NOCOLOR}"
+          sudo mkdir ${fullpath} && echo -e "${GREEN}...done${NOCOLOR}"
+          echo -e "${YELLOW}Extracting & moving downloaded java${JAVA_VERSION} binaries to ${DIRECTORY}java${JAVA_VERSION}${NOCOLOR}"
+          sudo tar xzf ./java_binaries/java${JAVA_VERSION} -C ${DIRECTORY}java${JAVA_VERSION} --strip-components 1 && echo -e "${GREEN}...done${NOCOLOR}"
+          JAVA_VERSION=$(($JAVA_VERSION+1))
+        done
         exit 1
-        echo "Extracting & Moving JDK 11 to /var/lib/java/java11..."
-        sudo tar xvzf java11
-        #sudo mv jdk-11.0.2 /var/lib/java/java11
-        echo "...done"
-        echo "Extracting & Moving JDK 12 to /var/lib/java/java11..."
-        sudo tar xvzf java12
-        #sudo mv jdk-11.0.2 /var/lib/java/java11
-        echo "...done"
-        echo "Extracting & Moving JDK 13 to /var/lib/java/java11..."
-        sudo tar xvzf java13
-        #sudo mv jdk-11.0.2 /var/lib/java/java11
-        echo "...done"
     }
     exit 1
     echo "Copying cjava command to /bin..."
